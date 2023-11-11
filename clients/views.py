507e5ede854bc4a6724aea427ffb7fbf1793c1d2
root <maresp85@gmail.com>
2023-11-11 18:12:38 -0500

    add get ip info

--- clients/views.py
+++ clients/views.py
@@ -39,11 +39,11 @@ def get_credit(request):
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
+        ip = x_forwarded_for.split(',')[1]        
     else:
         ip = request.META.get('REMOTE_ADDR')
     
-    return request.META.get('REMOTE_ADDR')
+    return ip
 
 
 def request_credit(request):
